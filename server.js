ae89ec7f8086175bcd84e007d879de813d9975bd
Youngnamez <etienneh78@gmail.com>
2021-04-28 12:18:37 -0400

    Addedfiles and created branch

---
+++ server.js
@@ -0,0 +1,46 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require("express")
+const bodyParser = require("body-parser")
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+// app.use(cors);
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+app.listen(8080, function() {
+    console.log("server running on port 8080");
+});
+
+app.get("/getWeatherData", function getWeatherData(req, res) {
+    console.log("Temperature is " + req.body.temperature);
+    console.log("Date is " + req.body.date);
+    console.log("User Response is " + req.body.userResponse);
+    res.send(projectData);
+});
+
+app.post("/postWeatherData", function (req, res) {
+    console.log("req \n")
+    console.log(req.body)
+    projectData = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        userResponse: req.body.userResponse
+    }
+    console.log(projectData);
+});
+
---
+++ website/app.js
@@ -0,0 +1,89 @@
+/* Global Variables */
+
+// Got past cors issue with help from this link: https://forum.freecodecamp.org/t/calling-openweathermap-api-is-blocked-due-to-cors-header-access-control-allow-origin-missing/191868
+const corsUrl = "https://cors-anywhere.herokuapp.com/";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "74b94287c045bb0a932cd33e7e5d3b74";
+let zipCode = "07050";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+async function getWeatherData (baseurl, zipCode, apiKey) {
+    const response = await fetch(baseurl + zipCode + ",us&appid=" + apiKey, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', 
+   });
+
+    try {
+      const newData = await response.json();
+      console.log(newData);
+             return newData.main.temp
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
+}
+
+async function postWeatherData (path, data) {
+    const response = await fetch(path, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', 
+    body: JSON.stringify(data),
+   });
+}
+
+async function updateUIWithData(path) {
+    const response = await fetch(path, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', 
+       });
+    console.log("helllo there")
+    //some work to update the UI
+    try {
+        const updatedData = await response.json();
+        console.log(updatedData);
+        document.getElementById('feelings').value = updatedData.userResponse;
+        document.getElementById('date').value = updatedData.date;
+        document.getElementById('temp').value = updatedData.temp;
+      }catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+      }
+}
+
+
+document.getElementById("generate").addEventListener('click', function() {
+
+    // leaving zip code as const variable for now
+    getWeatherData(corsUrl + apiUrl, zipCode, apiKey)
+    .then(function(tempResult) {
+        let data = {
+            temperature: tempResult,
+            date: document.getElementById('date').value,
+            userResponse: document.getElementById('feelings').value //big help from here: https://knowledge.udacity.com/questions/515780
+        }
+        console.log(data);
+        postWeatherData("/postWeatherData", data) }
+    )
+    .then(function(result) {
+        updateUIWithData("/getWeatherData", result)
+    })
+
+    // let resp = getWeatherData(corsUrl + apiUrl, zipCode, apiKey)
+    // .then(function(result) {
+    //     console.log(result);
+    //     postWeatherData("/postWeatherData", result);
+    // })
+    // .then(function() {
+    //     console.log("made it")
+    //     updatedServerData = getWeatherAndUpdateApp("/getWeatherData");
+        
+    //     document.getElementById("temp").innerText = "1";
+    //     document.getElementById("date").innerText = "1";
+    //     document.getElementById("content").innerText = "1";
+    // })
+    
+});
