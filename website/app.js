294bfc1206bbc4e414c46d3ae2befb7484cf65d7
Youngnamez <etienneh78@gmail.com>
2021-04-29 17:33:41 -0400

    adding current code

--- website/app.js
+++ website/app.js
@@ -1,30 +1,17 @@
 /* Global Variables */
 
-// Got past cors issue with help from this link: https://forum.freecodecamp.org/t/calling-openweathermap-api-is-blocked-due-to-cors-header-access-control-allow-origin-missing/191868
-// const corsUrl = "https://cors-anywhere.herokuapp.com/";
-const corsUrl = "";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
 const apiKey = "74b94287c045bb0a932cd33e7e5d3b74";
-let zipCode = "07050";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// Not sure why month comes out as the previous month and not the current
+let newDate = ++d.getMonth() +'.'+ d.getDate()+'.'+ d.getFullYear();
 
 
 async function getWeatherData (baseurl, zipCode, apiKey) {
-    console.log("hello")
     console.log(baseurl + zipCode + ",us&appid=" + apiKey);
-    const response = await fetch(baseurl + zipCode + ",us&appid=" + apiKey, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:8000/',
-    },
-    
-    mode: 'cors',
-    credentials: 'same-origin', 
-   });
+    const response = await fetch(baseurl + zipCode + ",us&appid=" + apiKey);
 
     try {
         const newData = await response.json();
@@ -33,7 +20,6 @@ async function getWeatherData (baseurl, zipCode, apiKey) {
     }
     catch(error) {
     console.log("error", error);
-    // appropriately handle the error
     }
 }
 
@@ -64,9 +50,9 @@ async function updateUIWithData(path) {
     try {
         const updatedData = await response.json();
         console.log(updatedData);
-        document.getElementById('feelings').value = updatedData.userResponse;
-        document.getElementById('date').value = updatedData.date;
-        document.getElementById('temp').value = updatedData.temp;
+        document.getElementById('content').innerHTML = updatedData.userResponse;
+        document.getElementById('date').innerHTML = updatedData.date;
+        document.getElementById('temp').innerHTML = updatedData.temperature;
       }catch(error) {
       console.log("error", error);
       // appropriately handle the error
@@ -75,13 +61,12 @@ async function updateUIWithData(path) {
 
 
 document.getElementById("generate").addEventListener('click', function() {
-
     // leaving zip code as const variable for now
-    getWeatherData(corsUrl + apiUrl, zipCode, apiKey)
+    getWeatherData(apiUrl, document.getElementById('zip').value, apiKey)
     .then(function(tempResult) {
         let data = {
             temperature: tempResult,
-            date: document.getElementById('date').value,
+            date: newDate,
             userResponse: document.getElementById('feelings').value //big help from here: https://knowledge.udacity.com/questions/515780
         }
         console.log(data);
@@ -90,19 +75,4 @@ document.getElementById("generate").addEventListener('click', function() {
     .then(function(result) {
         updateUIWithData("/getWeatherData", result)
     })
-
-    // let resp = getWeatherData(corsUrl + apiUrl, zipCode, apiKey)
-    // .then(function(result) {
-    //     console.log(result);
-    //     postWeatherData("/postWeatherData", result);
-    // })
-    // .then(function() {
-    //     console.log("made it")
-    //     updatedServerData = getWeatherAndUpdateApp("/getWeatherData");
-        
-    //     document.getElementById("temp").innerText = "1";
-    //     document.getElementById("date").innerText = "1";
-    //     document.getElementById("content").innerText = "1";
-    // })
-    
 });
